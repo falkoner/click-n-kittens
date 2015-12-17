39d8d5318e14291b5b725ccda38d081b74a6f8ff
Alex Filatau <filatau@gmail.com>
2015-12-16 22:43:14 -0800

    Convert cat view and cat list to KO object

--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,45 @@
-  window.onload = function() {
+var initialCats = [
+        {"name": "Cratty", "image": "img/cat1.jpg", "counter": 0},
+        {"name": "Pitty", "image": "img/cat2.jpg", "counter": 0},
+        {"name": "Gritty", "image": "img/cat3.jpg", "counter": 0},
+        {"name": "Shmitty", "image": "img/cat4.jpg", "counter": 0},
+        {"name": "Brunty", "image": "img/cat5.jpg", "counter": 0},
+        {"name": "Tritty", "image": "img/cat6.jpg", "counter": 0}
+]
+
+var Cat = function(data) {
+  this.name = ko.observable(data.name);
+  this.counter = ko.observable(data.counter);
+  this.image = ko.observable(data.image);
+}
+
+
+var ViewModel = function() {
+  var self = this;
+
+  self.catList = ko.observableArray([]);
+
+  initialCats.forEach( function(catItem){
+    self.catList.push(new Cat(catItem));
+  });
+
+  self.currentCat = ko.observable(self.catList()[0]);
+
+  self.incrementCount = function() {
+      self.currentCat().counter(self.currentCat().counter() + 1);
+  };
+
+  self.changeCat = function (newCat) {
+    self.currentCat(newCat);
+  }
+};
+
+ko.applyBindings(new ViewModel());
+
+
+
+
+  // window.onload = function() {
 
     // cats data structure
     var model = {
@@ -135,6 +176,6 @@
       }
     };
 
-    octopus.init();
+    // octopus.init();
 
-  };
+  // };
